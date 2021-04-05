6ce7bd9654aa21021a7980cb46ad0fa8d6fcbd8e
Krishaay Jois <62088268+krishaayjois21@users.noreply.github.com>
2021-04-05 14:19:42 +0530

    Files for vector generators

---
+++ generate_vectors.py
@@ -0,0 +1,19 @@
+import sys
+from app import __version__
+vectors_name = {
+    1: 'Nitro Generator',
+    2: 'Nitro Checker',
+    3: 'Nitro Gen + Checker'
+}
+
+print(
+    f'''
+           ___  __  ___  __   __      __   ___      
+     \  / |__  /  `  |  /  \ |__)    / _` |__  |\ | 
+      \/  |___ \__,  |  \__/ |  \    \__> |___ | \| 
+                    v{__version__}
+        https://github.com/krishaayjois21/Quintex
+    '''
+)
+
+print()
